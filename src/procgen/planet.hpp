62c5f3ae2740aa2ac7fc321db2f578e76b075d74
theTacocat88 <eliasglick1@gmail.com>
2026-08-28 07:46:01 -0400

    fix(planet): change mergePlanetFaceData to accept const reference for data array for safety

--- src/procgen/planet.hpp
+++ src/procgen/planet.hpp
@@ -20,7 +20,7 @@ MeshData generatePlanetFace(int res, const FastNoiseLite& noise, float noiseScal
 std::array<MeshData, 6> createPlanetMeshDataGroup(int res, const FastNoiseLite& noise, float noiseScale, float heightScale);
 
 // Merge the data from the function above
-MeshData mergePlanetFaceData(std::array<MeshData, 6>& data);
+MeshData mergePlanetFaceData(const std::array<MeshData, 6>& data);
 
 // Link all these functions together to create a unified mesh
 Mesh createPlanetMesh(int res, const FastNoiseLite& noise, float noiseScale, float heightScale);
